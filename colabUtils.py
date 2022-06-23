5fe56080ecd4b5c935de43db152645313bc3af92
Waleed-Mujahid <mmujahid.bscs20seecs@seecs.edu.pk>
2022-06-23 11:56:09 +0500

    added image preprocessing

--- colabUtils.py
+++ colabUtils.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import tensorflow as tf
+from PIL import image 
 
 def plotLoss(history, validation = False, height = 7, width = 4):
     plt.figure(figsize = (height,width))
@@ -23,3 +25,20 @@ def plotAccuracy(history, validation = False, height = 7, width = 4):
         plt.plot(history.history['val_accuracy'])
         plt.legend(['Train', 'Test'], loc='lower right')
     plt.show()
+
+def processImg(path,target_shape,show=False,grayscale=False ):
+    if (show):
+        _img = Image.open(path)    # Visualizing the image
+        _img.load()
+        plt.xticks([])             # Disabling ticks
+        plt.yticks([])
+        plt.imshow(_img)
+
+    # Converting the image so it can be fed into the model
+    img = tf.io.read_file(path)
+    img = tf.image.decode_image(img)
+    test_img = tf.image.resize(img, size = target_shape)    # Resize the image according to the model
+    if (grayscale):
+        test_img = tf.image.rgb_to_grayscale(test_img)                     # Model expects a grayscale image
+    test_img = test_img/255.                                           # Normalizing the image
+    return test_img
