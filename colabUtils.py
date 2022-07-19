646a1e932991ecaf12a9e845919c1e9e480a261a
Waleed-Mujahid <mmujahid.bscs20seecs@seecs.edu.pk>
2022-07-19 10:19:33 +0500

    colabUtils updated, added visualize data func

--- colabUtils.py
+++ colabUtils.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from PIL import Image 
+import os,random
 
-def plotLoss(history, validation = False, height = 7, width = 4):
-    plt.figure(figsize = (height,width))
+def plotLoss(history, validation = False, dims=(7,4)):
+    plt.figure(figsize = dims)
     plt.plot(history.history['loss'])    
     plt.title('Loss Curve')
     plt.ylabel('Loss')
@@ -15,8 +16,8 @@ def plotLoss(history, validation = False, height = 7, width = 4):
     #     plt.legend(['Train'], loc='upper right')
     plt.show()
 
-def plotAccuracy(history, validation = False, height = 7, width = 4):
-    plt.figure(figsize = (height,width))
+def plotAccuracy(history, validation = False, dims=(7,4)):
+    plt.figure(figsize = dims)
     plt.plot(history.history['accuracy'])
     plt.title('Model Accuracy')
     plt.ylabel('Accuracy')
@@ -42,3 +43,20 @@ def processImg(path,target_shape,show=False,grayscale=False ):
         test_img = tf.image.rgb_to_grayscale(test_img)                     # Model expects a grayscale image
     test_img = test_img/255.                                           # Normalizing the image
     return test_img
+
+def visualizeData(filepath, dims, fig_size = (10,5)):
+    plt.figure(figsize = fig_size)
+    count = 1
+    for folder in os.listdir(filepath):
+        path = filepath + '/' + folder
+        img = random.choice(os.listdir(path))
+        img_path = path + '/' + img
+        plt.subplot(dims[0],dims[1],count)
+        img = Image.open(img_path)    # Visualizing the image
+        img.load()
+        plt.xticks([])             # Disabling ticks
+        plt.yticks([])
+        plt.title(folder)
+        plt.imshow(img, cmap = 'gray')
+        count += 1  
+        img_width, img_height = img.size
